04a34ebde6f5b9792d7e1aa73fc1f24b01eaa5c7
Deep Datta <171801242+dxdprojects@users.noreply.github.com>
2026-01-18 22:29:53 +0530

    Update docker push command to include latest tag

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -2,6 +2,6 @@ def call(String credId, String imageName){
   withCredentials([usernamePassword(credentialsId: "${credId}", passwordVariable: "dockerPass", usernameVariable: "dockerUser")]){
                     sh "docker login -u ${env.dockerUser} -p ${env.dockerPass} "
                     sh "docker image tag ${imageName} ${env.dockerUser}/${imageName}"
-                    sh "docker push ${env.dockerUser}/${imageName}"
+                    sh "docker push ${env.dockerUser}/${imageName}:latest"
                 }
 }
