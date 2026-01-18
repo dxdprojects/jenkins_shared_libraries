72b36fc73081ab87546a82356248b9a2b09026f9
Deep Datta <171801242+dxdprojects@users.noreply.github.com>
2026-01-18 22:10:31 +0530

    Create docker_push.groovy

---
+++ vars/docker_push.groovy
@@ -0,0 +1,7 @@
+def call(String credId, String imageName){
+  withCredentials([usernamePassword(credentialsId: "${credId}", passwordVariable: "dockerPass", usernameVariable: "dockerUser")]){
+                    sh "docker login -u ${env.dockerUser} -p ${env.dockerPass} "
+                    sh "docker image tag "${imageName}" ${env.dockerUser}/${imageName}"
+                    sh "docker push ${env.dockerUser}/${imageName}"
+                }
+}
